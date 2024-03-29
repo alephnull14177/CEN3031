e402157ddd7a8da0f08fb225080244f6a3f9b15c
Anteneh Zewdie <kingzewdie16@gmail.com>
2024-03-29 01:12:17 -0400

    Revert "Revert "Merge pull request #4 from alephnull14177/Anteneh-pull""
    This reverts commit 1acbcdcce5dc9257ca9e280e356a4d983ef39214.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Navbar from './components/Navbar'
+import Events from './pages/Events'
 
 function App() {
   const { user } = useAuthContext()
@@ -30,6 +31,10 @@ function App() {
             <Route 
               path="/signup" 
               element={!user ? <Signup /> : <Navigate to="/" />} 
+            />
+            <Route
+              path="/Events"
+              element={user ? <Events /> : <Navigate to="/Events" />}
             />
           </Routes>
         </div>
