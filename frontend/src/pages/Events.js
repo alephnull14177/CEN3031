fc0444c302319dab1a81e04329cc514bd6eada87
Aarushi Jain <aarushijain@gmail.com>
2024-03-31 21:43:00 -0400

    added form on events page

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,59 +1,34 @@
+import React, { useState, useEffect } from "react";
+import EventForm from '../components/EventForm';
 import { useAuthContext } from "../hooks/useAuthContext";
-import { useEffect } from "react";
-import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
-
-// components
-import WorkoutDetails from '../components/WorkoutDetails'
-import WorkoutForm from '../components/WorkoutForm'
 
 const Events = () => {
-  // const {workouts, dispatch} = useWorkoutsContext()
   const { user } = useAuthContext();
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     document.title = "Events | Downtown Volunteers";
+  }, []);
 
-    // const fetchWorkouts = async () => {
-    //   const response = await fetch('/api/workouts', {
-    //     headers: {'Authorization': `Bearer ${user.token}`},
-    //   })
-    //   const json = await response.json()
-
-    //   if (response.ok) {
-    //     dispatch({type: 'SET_WORKOUTS', payload: json})
-    //   }
-    // }
-
-    // if (user) {
-    //   fetchWorkouts()
-    // }
-
-  });//, [dispatch, user]);
+  const addEvent = (newEvent) => {
+    setEvents((prevEvents) => [...prevEvents, newEvent]);
+  };
 
   return (
     <div className="events">
       <h2>Upcoming Events</h2>
       <div className="event-list">
-        <div className="event">
-          <h3>Event Title 1</h3>
-          <p>Date: March 30, 2024</p>
-          <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div className="event">
-          <h3>Event Title 2</h3>
-          <p>Date: April 5, 2024</p>
-          <p>Description: Nulla facilisi. Phasellus fermentum ex ac lectus fermentum, vel.</p>
-        </div>
-        {/* Add more events as needed */}
-      </div>
-      {/* <div className="workouts">
-        {workouts && workouts.map((workout) => (
-          <WorkoutDetails key={workout._id} workout={workout} />
+        {events.map((event, index) => (
+          <div className="event" key={index}>
+            <h3>{event.title}</h3>
+            <p>Date: {event.date}</p>
+            <p>Description: {event.description}</p>
+          </div>
         ))}
       </div>
-      <WorkoutForm />  */}
+      {user && <EventForm addEvent={addEvent} />}
     </div>
   );
-}
+};
 
 export default Events;
